fb0f62cbcc35ef41e404396ff0c865589e25d053
poran-dip <porandip0@gmail.com>
2026-03-13 13:44:51 +0530

    feat: change routes and signin

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -3,5 +3,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("/auth", "routes/auth.tsx"),
-  route("/trust", "routes/trust.tsx")
+  route("/check", "routes/trust.tsx")
 ] satisfies RouteConfig;
